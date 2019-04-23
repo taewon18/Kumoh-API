bed1849105bc87e66fc1e2fc96ed27ccfedc8a1f
taewon <htw1800@kumoh.ac.kr>
2019-04-23 10:22:52 +0900

    2019-04-23 10:22 홍태원 교직원 식당 식단 정보 제공 라우터 작성

--- routes/se.js
+++ routes/se.js
@@ -91,4 +91,85 @@ router.get('/getStudentRestaurantMenu', function(request, response, next) {
     });
 });
 
+//교직원 식당 식단 정보 조회
+router.get('/getEmployeeRestaurantMenu', function(request, response, next) {
+    var getDate = request.param("date");
+    var addressDate;
+    var studentRestaurantAddress
+    momentObject.format('YYYY-MM-DD');
+    
+    if(getDate == undefined) {
+        addressDate = momentObject.format('YYYY-MM-DD');
+        //클라이언트로 데이터 수신이 되지 않으면 오늘 날짜로 설정
+    } else {
+        //클라이언트로 부터 데이터 수신 시 날짜를 입력해준다.
+        addressDate = getDate;
+    }
+
+    if(moment(addressDate).day() == 0) {
+        studentRestaurantAddress = "http://www.kumoh.ac.kr/ko/restaurant02.do?mode=menuList&srDt=" + moment(addressDate).subtract(1,"days").format("YYYY-MM-DD");
+        addressDate = moment(addressDate).subtract(1,"days").format("YYYY-MM-DD");
+    } else {
+        studentRestaurantAddress = "http://www.kumoh.ac.kr/ko/restaurant02.do?mode=menuList&srDt=" + addressDate;
+    }
+
+    var day = moment(addressDate).day();
+
+    cheerio.fetch(studentRestaurantAddress, param, function(err, $, res){ 
+        if(err){ 
+            console.log(err); 
+            return; 
+        }
+
+        var jsonArray = new Array();
+        var day_count = 1;
+        var meal_count = 2;
+        
+        $(".s-dot").each( function() {
+            //console.log($(this).find('li').length);
+            var select_date;
+
+            if(day > day_count){
+                select_date = moment(addressDate).subtract(day-day_count,"days").format("YYYY-MM-DD");
+            } else if(day == day_count) {
+                select_date = moment(addressDate).format("YYYY-MM-DD");
+            } else if(day < day_count){
+                select_date = moment(addressDate).add(day_count-day,"days").format("YYYY-MM-DD");
+            }
+
+            var array = [];
+            var jsonObject = new Object();
+            jsonObject.date = select_date;
+            jsonObject.meal_count = meal_count;
+            //var menu_index = 0;
+            $(this).find('li').each( function() {
+                //console.log(menu_index + " : " + $(this).text())
+                array.push($(this).text());
+                //menu_index++;
+            });
+            jsonObject.count = $(this).find('li').length;
+            jsonObject.menu = array;
+            jsonArray.push(jsonObject);
+
+            if(day_count == 7) {
+                day_count = 1;
+                meal_count++;
+            } else {
+                day_count++;
+            }
+        });
+
+        var selectArray = new Array();
+        
+        for(var i = 0; i < jsonArray.length; i++) {
+            if(jsonArray[i].date == getDate) {
+                selectArray.push(jsonArray[i]);
+            }
+        }
+        
+        response.send(JSON.stringify(selectArray));
+    });
+});
+
+
 module.exports = router;
